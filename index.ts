f8d04b8add275a36f04fd02a5abe6a58840415cc
Kenneth <kennethnym@outlook.com>
2024-09-25 22:03:21 +0100

    feat: allow port to be overriden via env

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get("/wallpapers", async (req, res) => {
 	const dataFile = Bun.file("./data.json");
